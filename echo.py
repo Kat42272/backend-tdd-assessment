6487222c7ec100463f740e47bb68874f2bb6b9fa
Kathryn Anderson <kat42272@yahoo.com>
2020-06-23 10:11:38 -0700

    Updated file to include all tests

--- echo.py
+++ echo.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility."""
 
-__author__ = "Student Study group led by Daniel Lomelino, written by Kathryn Anderson"
+__author__ = "Student Study group led by Daniel Lomelino, and Jo Anna Mollman, written by Kathryn Anderson"
 
 
 import sys
@@ -12,49 +12,46 @@ import argparse
 def create_parser():
     """Returns an instance of argparse.ArgumentParser"""
     parser = argparse.ArgumentParser(
-        description="Transform and display a message.")
-    parser.add_argument('msg', help="message to display")
-    parser.add_argument('-b', '--border', 
-        help="border character around message")
-    parser.add_argument('-n', type=int,
-        help="number of times to display the message")
-    parser.add_argument('-u', action='store_true',
-        help="transform message to uppercase")
-    parser.add_argument('-l', action='store_true',
-        help="transform message to lowercase")
-    parser.add_argument('-t', action='store_true',
-        help="message to display")
+        description="Perform transformation on input text.")
+    # parser.add_argument('msg', help="message to display")
+    parser.add_argument('-u', "--upper", action='store_true',
+        help="convert text to uppercase")
+    parser.add_argument('-l', "--lower", action='store_true',
+        help="convert text to lowercase")
+    parser.add_argument('-t', "--title", action='store_true',
+        help="convert text to titlecase")
+    parser.add_argument('text', help="text to be manipulated")
     return parser
 
 
 def main(args):
     """Implementation of echo"""
     parser = create_parser()
-    ns = parser.parse_args()
+    ns = parser.parse_args(args)
 
-    num_times = ns.n or 1
-    msg = ns.msg
+    # num_times = ns.n or 1
+    text = ns.text
 
     # apply command line argument logic
-    if ns.u:
-        msg = msg.upper()
-    if ns.l:
-        msg = msg.lower()
-    if ns.t:
-        msg = msg.title()
 
-    # display the tranformed message
-    if ns.border:
-        print(f"{ns.border * len(msg)}")
-    for n in range(num_times):
-        print(msg)
-    if ns.border:
-        print(f"{ns.border * len(msg)}")
+    if ns.upper:
+        text = text.upper()
+    if ns.lower:
+        text = text.lower()
+    if ns.title:
+        text = text.title()
+    print(text)
+
+    # # display the tranformed message
+    # if ns.border:
+    #     print(f"{ns.border * len(msg)}")
+    # for n in range(num_times):
+    #     print(msg)
+    # if ns.border:
+    #     print(f"{ns.border * len(msg)}")
 
 
-
-
-    return
+    return text
 
 
 if __name__ == '__main__':
